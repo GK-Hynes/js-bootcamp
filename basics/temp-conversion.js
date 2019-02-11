bc9ed5ae94c7317cbb55906d2aefe5354f68cb0b
Gerard Hynes <gkhynes@gmail.com>
2019-02-11 21:52:43 +0000

    Add first exercise

---
+++ basics/temp-conversion.js
@@ -0,0 +1,9 @@
+let fahrenheit = 32;
+
+// Calculate celsius
+let celsius = (fahrenheit - 32) * (9 / 5);
+console.log(celsius);
+
+// Calculate kelvin
+let kelvin = (fahrenheit + 459.67) * (5 / 9);
+console.log(kelvin);
