e9ede54db29c452fcbf03e3be044a4d5f1ffd6a0
Gerard Hynes <gkhynes@gmail.com>
2019-02-18 21:49:16 +0000

    Add boolean.js

---
+++ basics/boolean.js
@@ -0,0 +1,29 @@
+// === - equality operator
+// !== - inequality operator
+// <   - less than operator
+// >   - greater than operator
+// <=  - less than or equal to operator
+// >=  - greater than or equal to operator
+
+let temp = 115;
+
+if (temp <= 32) {
+  console.log("It is freezing outside!");
+}
+
+if (temp >= 110) {
+  console.log("It is way too hot outside!");
+}
+
+// Challenge
+
+// Create age set to your age
+let age = 31;
+
+if (age <= 7) {
+  console.log("Child discount");
+}
+
+if (age >= 65) {
+  console.log("Senior discount");
+}
