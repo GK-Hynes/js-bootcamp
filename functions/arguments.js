908ddf70b5a766c736abce98233a1cbc42effebf
Gerard Hynes <gkhynes@gmail.com>
2019-02-25 21:35:30 +0000

    Update arguments.js

--- functions/arguments.js
+++ functions/arguments.js
@@ -9,7 +9,7 @@ console.log(result);
 
 // Default arguments
 let getScoreText = function(name = "Anonymous", score = 0) {
-  return "Name: " + name + " - Score: " + score;
+  return `Name: ${name} = Score: ${score}`;
 };
 
 let scoreText = getScoreText(undefined, 7);
@@ -17,9 +17,13 @@ let scoreText = getScoreText(undefined, 7);
 console.log(scoreText);
 
 // Challenge
+// Return string: A 20% tip on €50 would be €10
 let getTip = function(total, tipPercent = 0.2) {
-  return total * tipPercent;
+  let percent = tipPercent * 100;
+  let tip = total * tipPercent;
+  return `A ${percent}% tip on €${total} would be €${tip}`;
+  // return `A ${tipPercent * 100}% tip on €${total} would be €${total * tipPercent}`;
 };
 
-let tip = getTip(100, 0.15);
+let tip = getTip(50, 0.2);
 console.log(tip);
