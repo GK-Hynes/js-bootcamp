2c76bb461a0839bafa31ca50bb3d63490976d2a4
Gerard Hynes <gkhynes@gmail.com>
2019-03-03 18:14:01 +0000

    Practice using forEach

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,12 +1,12 @@
 const todos = ["make breakfast", "yoga", "shower", "programme", "walk"];
 
-// Delete the 3rd item
-// Add a new item to the end
-// Remove thr first item
-
 todos.splice(2, 1);
 todos.push("dancing practice");
 todos.shift();
 
 console.log(`You have ${todos.length} todos`);
-console.log(todos);
+
+// Print each item's index and content
+todos.forEach(function(todo, index) {
+  console.log(`${index + 1}. ${todo}`);
+});
