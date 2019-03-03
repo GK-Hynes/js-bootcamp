64cecf1768d736305d0d094e53b425f0c9bd1f2e
Gerard Hynes <gkhynes@gmail.com>
2019-03-03 10:28:17 +0000

    Add notes.js and todo.js

---
+++ arrays/todo.js
@@ -0,0 +1,10 @@
+// Create an array with 5 todos
+// Print You have x todos
+// Print first and second to last item -> Todo:
+
+const todos = ["make breakfast", "yoga", "shower", "programme", "walk"];
+
+console.log(`You have ${todos.length} todos`);
+console.log(
+  `First todo: ${todos[0]}, Second to last todo: ${todos[todos.length - 2]}`
+);
