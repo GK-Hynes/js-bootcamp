ee0a2d5baf3e50a8e5a6d071f8e86e8e91422338
Gerard Hynes <gkhynes@gmail.com>
2019-03-03 18:01:04 +0000

    Practice array methods

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,10 +1,12 @@
-// Create an array with 5 todos
-// Print You have x todos
-// Print first and second to last item -> Todo:
-
 const todos = ["make breakfast", "yoga", "shower", "programme", "walk"];
 
+// Delete the 3rd item
+// Add a new item to the end
+// Remove thr first item
+
+todos.splice(2, 1);
+todos.push("dancing practice");
+todos.shift();
+
 console.log(`You have ${todos.length} todos`);
-console.log(
-  `First todo: ${todos[0]}, Second to last todo: ${todos[todos.length - 2]}`
-);
+console.log(todos);
