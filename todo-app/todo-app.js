283e8a245cca171c482b6cbfa2719dde6c041f8a
Gerard Hynes <gkhynes@gmail.com>
2019-03-10 15:21:41 +0000

    Filter and add todos

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,24 @@
-const ps = document.querySelectorAll("p");
+const todos = [
+  { text: "make breakfast", completed: true },
+  { text: "yoga", completed: false },
+  { text: "shower", completed: false },
+  { text: "programme", completed: true },
+  { text: "walk", completed: false }
+];
 
-ps.forEach(function(p) {
-  if (p.textContent.toLowerCase().includes("the")) {
-    p.remove();
-  }
+// Print summary - You have 2 todos left
+// Add p for each todo
+
+const incompleteTodos = todos.filter(function(todo) {
+  return !todo.completed;
+});
+
+const summary = document.createElement("h2");
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector("body").appendChild(summary);
+
+todos.forEach(function(todo) {
+  const newTodo = document.createElement("p");
+  newTodo.textContent = todo.text;
+  document.querySelector("body").appendChild(newTodo);
 });
