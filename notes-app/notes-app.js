6c17e45d75d0d68ac6922ce18cdb0d9df415613f
Gerard Hynes <gkhynes@gmail.com>
2019-03-10 14:32:31 +0000

    Practice DOM manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+// DOM - Document Object Model
+// const p = document.querySelector("p");
+// p.remove();
+
+const ps = document.querySelectorAll("p");
+ps.forEach(function(p) {
+  p.textContent = "*****";
+  // console.log(p.textContent);
+  // p.remove();
+});
