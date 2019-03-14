e856a16e896cfdecee77eb0460286e982ca27e17
Gerard Hynes <gkhynes@gmail.com>
2019-03-14 20:16:02 +0000

    Practice working with localStorage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -17,6 +17,22 @@ const filters = {
   searchText: ""
 };
 
+// const user = {
+//   name: "Joe",
+//   age: 29
+// };
+
+// userJSON = JSON.stringify(user);
+
+// console.log(userJSON);
+
+// localStorage.setItem("user", userJSON);
+
+const userJSON = localStorage.getItem("user");
+const user = JSON.parse(userJSON);
+
+console.log(`${user.name} is ${user.age}`);
+
 const renderNotes = function(notes, filters) {
   const filteredNotes = notes.filter(function(note) {
     return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
