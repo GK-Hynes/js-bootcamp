549e917b70c676b8127115dba46e06de8d8e1341
Gerard Hynes <gkhynes@gmail.com>
2019-03-15 21:02:56 +0000

    Refactor notes app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,52 +1,31 @@
-let notes = [];
+// Get notes from localStorage
+const notes = getSavedNotes();
 
+// Filters
 const filters = {
   searchText: ""
 };
 
-// Check for existing saved data
-const notesJSON = localStorage.getItem("notes");
-
-if (notesJSON !== null) {
-  notes = JSON.parse(notesJSON);
-}
-
-const renderNotes = function(notes, filters) {
-  const filteredNotes = notes.filter(function(note) {
-    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-
-  document.querySelector("#notes").innerHTML = "";
-
-  filteredNotes.forEach(function(note) {
-    const noteElement = document.createElement("p");
-
-    if (note.title.length > 0) {
-      noteElement.textContent = note.title;
-    } else {
-      noteElement.textContent = "Unnamed note";
-    }
-
-    document.querySelector("#notes").appendChild(noteElement);
-  });
-};
-
+// Render notes to the page
 renderNotes(notes, filters);
 
+// Add new note
 document.querySelector("#create-note").addEventListener("click", function(e) {
   notes.push({
     title: "",
     body: ""
   });
-  localStorage.setItem("notes", JSON.stringify(notes));
+  saveNotes(notes);
   renderNotes(notes, filters);
 });
 
+// Search notes
 document.querySelector("#search-text").addEventListener("input", function(e) {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
 
+// Filter notes
 document.querySelector("#filter-by").addEventListener("change", function(e) {
   console.log(e.target.value);
 });
---
+++ notes-app/notes-functions.js
@@ -0,0 +1,42 @@
+// Read notes from localStorage
+const getSavedNotes = function() {
+  const notesJSON = localStorage.getItem("notes");
+
+  if (notesJSON !== null) {
+    return JSON.parse(notesJSON);
+  } else {
+    return [];
+  }
+};
+
+// Save notes to localStorage
+const saveNotes = function(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+// Generate DOM structure for a note
+const generateNoteDOM = function(note) {
+  const noteElement = document.createElement("p");
+
+  if (note.title.length > 0) {
+    noteElement.textContent = note.title;
+  } else {
+    noteElement.textContent = "Unnamed note";
+  }
+
+  return noteElement;
+};
+
+// Render notes to the page
+const renderNotes = function(notes, filters) {
+  const filteredNotes = notes.filter(function(note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+
+  document.querySelector("#notes").innerHTML = "";
+
+  filteredNotes.forEach(function(note) {
+    const noteElement = generateNoteDOM(note);
+    document.querySelector("#notes").appendChild(noteElement);
+  });
+};
