c2f4fa63efd6c2178a8115cc05dd1ff129166c78
Gerard Hynes <gkhynes@gmail.com>
2019-03-15 21:28:24 +0000

    Refactor todo app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,5 +1,5 @@
 // Todos
-let todos = [];
+const todos = getSavedTodos();
 
 // Filters
 filters = {
@@ -7,41 +7,7 @@ filters = {
   hideCompleted: false
 };
 
-// Check for existing todos
-const todosJSON = localStorage.getItem("todos");
-
-if (todosJSON !== null) {
-  todos = JSON.parse(todosJSON);
-}
-
 // Render todos to the page
-const renderTodos = function(todos, filters) {
-  const filteredTodos = todos.filter(function(todo) {
-    const searchTextMatch = todo.text
-      .toLowerCase()
-      .includes(filters.searchText.toLowerCase());
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
-    return searchTextMatch && hideCompletedMatch;
-  });
-
-  const incompleteTodos = filteredTodos.filter(function(todo) {
-    return !todo.completed;
-  });
-
-  document.querySelector("#todos").innerHTML = "";
-
-  const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteTodos.length} todos left`;
-  document.querySelector("#todos").appendChild(summary);
-
-  filteredTodos.forEach(function(todo) {
-    const p = document.createElement("p");
-    p.textContent = todo.text;
-    document.querySelector("#todos").appendChild(p);
-  });
-};
-
 renderTodos(todos, filters);
 
 // Filter todos
@@ -55,7 +21,7 @@ document.querySelector("#new-todo").addEventListener("submit", function(e) {
   e.preventDefault();
   const newTodo = e.target.elements.newTodo.value;
   todos.push({ text: newTodo, completed: false });
-  localStorage.setItem("todos", JSON.stringify(todos));
+  saveTodos(todos);
   renderTodos(todos, filters);
   e.target.elements.newTodo.value = "";
 });
