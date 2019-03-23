17ca16e47d5f1d4bcadabc471b1ae4e5502fe065
Gerard Hynes <gkhynes@gmail.com>
2019-03-23 19:58:18 +0000

    Build basics of hangman game

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,25 @@
-const Hangman = function(word, guesses) {
-  this.word = word;
-  this.guesses = guesses;
+const Hangman = function(word, remainingGuesses) {
+  this.word = word.toLowerCase().split("");
+  this.remainingGuesses = remainingGuesses;
+  this.guessedLetters = ["c", "e"];
+};
+
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = "";
+
+  this.word.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === " ") {
+      puzzle += letter;
+    } else {
+      puzzle += "*";
+    }
+  });
+
+  return puzzle;
 };
 
 const game1 = new Hangman("Cairo", 3);
-console.log(game1);
+console.log(game1.getPuzzle());
 
 const game2 = new Hangman("Budapest", 4);
-console.log(game2);
+console.log(game2.getPuzzle());
