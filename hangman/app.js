f072fbfe5a55c74160cb5195a923f704d37c4497
Gerard Hynes <gkhynes@gmail.com>
2019-03-27 21:07:02 +0000

    Add calculateStatus method

--- hangman/app.js
+++ hangman/app.js
@@ -4,10 +4,12 @@ const game1 = new Hangman("Cairo", 3);
 
 puzzleElement.textContent = game1.getPuzzle();
 guessesElement.textContent = game1.remainingGuesses;
+console.log(game1.status);
 
 window.addEventListener("keypress", function(e) {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   puzzleElement.textContent = game1.getPuzzle();
   guessesElement.textContent = game1.remainingGuesses;
+  console.log(game1.status);
 });
