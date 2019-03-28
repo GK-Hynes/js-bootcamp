de45babeffb239e8e8ecbd3f5ff002b17e07b228
Gerard Hynes <gkhynes@gmail.com>
2019-03-28 20:16:43 +0000

    Refactor Hangman to use class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,25 +1,25 @@
-// Prototypal inheritance
+// Prototypal inheritance and Classes
 
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
-
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} is ${this.age}.`;
-  this.likes.forEach(like => {
-    bio += ` ${this.firstName} likes ${like}`;
-  });
-  return bio;
-};
-
-Person.prototype.setName = function(fullName) {
-  const names = fullName.split(" ");
-  this.firstName = names[0];
-  this.lastName = names[1];
-};
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`;
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}`;
+    });
+    return bio;
+  }
+  setName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
 
 const me = new Person("Aerith", "FF", 18, ["Biking", "Reading"]);
 me.setName("Hermione Granger");
