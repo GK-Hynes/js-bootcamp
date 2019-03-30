e1b04ffc13ec1ee7442a0a240a7fb3216fa495c1
Gerard Hynes <gkhynes@gmail.com>
2019-03-30 10:30:49 +0000

    Practice making HTTP requests

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request
+// Response
+
 const puzzleElement = document.querySelector("#puzzle");
 const guessesElement = document.querySelector("#guesses");
 const game1 = new Hangman("New York", 3);
@@ -11,3 +15,15 @@ window.addEventListener("keypress", function(e) {
   puzzleElement.textContent = game1.puzzle;
   guessesElement.textContent = game1.statusMessage;
 });
+
+// Making an HTTP Request
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", e => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  }
+});
+request.open("GET", "http://puzzle.mead.io/puzzle");
+request.send();
