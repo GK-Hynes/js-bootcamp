bb7a26847ea3727b02c69ca4e42edc01e2e43e35
Gerard Hynes <gkhynes@gmail.com>
2019-04-03 21:52:22 +0100

    Convert getCountry and getLocation to async functions

--- hangman/requests.js
+++ hangman/requests.js
@@ -10,30 +10,21 @@ const getPuzzle = async wordCount => {
   }
 };
 
-const getCountry = countryCode => {
-  return fetch(`http://restcountries.eu/rest/v2/all`)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error("Unable to fetch countries");
-      }
-    })
-    .then(data => {
-      return data.find(country => country.alpha2Code === countryCode);
-    });
+const getCountry = async countryCode => {
+  const response = await fetch(`http://restcountries.eu/rest/v2/all`);
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find(country => country.alpha2Code === countryCode);
+  } else {
+    throw new Error("Unable to fetch country");
+  }
 };
 
-const getLocation = () => {
-  return fetch(`http://ipinfo.io/json?token=5e408f897c1e10`)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error("Unable to fetch location");
-      }
-    })
-    .then(data => {
-      return data;
-    });
+const getLocation = async () => {
+  const response = await fetch(`http://ipinfo.io/json?token=5e408f897c1e10`);
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error("Unable to fetch location");
+  }
 };
