50732a5c4d4493cf9d07990d9beb367092dbcf32
Gerard Hynes <gkhynes@gmail.com>
2019-04-07 15:02:16 +0100

    Set up webpack-dev-server

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -2,7 +2,7 @@ import square, { add, name } from "./utilities";
 import scream from "./scream";
 
 console.log("index.js");
-console.log(add(33, 51));
+console.log(add(31, 9));
 console.log(name);
 console.log(scream(name));
 console.log(square(10));
