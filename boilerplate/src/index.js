dfba5b46e54fac564d555051cc5a6987429ed54c
Gerard Hynes <gkhynes@gmail.com>
2019-04-07 17:21:44 +0100

    Refactor Hangman app to use Webpack and Babel

--- boilerplate/src/index.js
+++
@@ -1,8 +0,0 @@
-import square, { add, name } from "./utilities";
-import scream from "./scream";
-
-console.log("index.js");
-console.log(add(31, 9));
-console.log(name);
-console.log(scream(name));
-console.log(square(10));
