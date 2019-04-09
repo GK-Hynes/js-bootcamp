1f30a9c97e7d4afe2cd984cdb3cb53a90ce2ba14
Gerard Hynes <gkhynes@gmail.com>
2019-04-09 22:37:56 +0100

    Import uuid and start setting up todos.js

--- todo-app/src/todos.js
+++ todo-app/src/todos.js
@@ -25,3 +25,39 @@
 // Return value: none
 
 // Make sure to call loadTodos and setup the exports
+
+import uuidv4 from "uuid/v4";
+
+let todos = [];
+
+const loadTodos = () => {
+  const todosJSON = localStorage.getItem("todos");
+
+  try {
+    return todosJSON ? JSON.parse(todosJSON) : [];
+  } catch (e) {
+    return [];
+  }
+};
+
+// Save todos to localStorage
+const saveTodos = todos => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+// Expose todos from module
+const getTodos = () => todos;
+
+const createTodo = text => {
+  if (text.length > 0) {
+    todos.push({
+      id: uuidv4(),
+      text: text,
+      completed: false
+    });
+    saveTodos();
+    renderTodos(todos, filters);
+  }
+};
+
+export { loadTodos, saveTodos, getTodos, createTodo, removeTodo, toggleTodo };
