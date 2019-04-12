fd18c2477fa4b16deab380f76ca4628192bbd6ed
Gerard Hynes <gkhynes@gmail.com>
2019-04-12 21:27:05 +0100

    Practice working with spread syntax

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,18 +1,36 @@
-const calculateAverage = (target, ...numbers) => {
-  let sum = 0;
-  numbers.forEach(num => {
-    sum += num;
-  });
-  const average = sum / numbers.length;
-  return `The average ${target} is ${average}`;
-};
+// const calculateAverage = (target, ...numbers) => {
+//   let sum = 0;
+//   numbers.forEach(num => {
+//     sum += num;
+//   });
+//   const average = sum / numbers.length;
+//   return `The average ${target} is ${average}`;
+// };
 
-console.log(calculateAverage("grade", 0, 100, 23, 75));
+// console.log(calculateAverage("grade", 0, 100, 23, 75));
 
-const printTeam = (team, coach, ...players) => {
-  console.log(`Team: ${team}`);
+// const printTeam = (team, coach, ...players) => {
+//   console.log(`Team: ${team}`);
+//   console.log(`Coach: ${coach}`);
+//   console.log(`Players: ${players.join(", ")}`);
+// };
+
+// printTeam("Kepler", "Beth", "Joe", "Susie", "Karen", "Saron");
+
+const printTeam = (teamName, coach, ...players) => {
+  console.log(`Team: ${teamName}`);
   console.log(`Coach: ${coach}`);
   console.log(`Players: ${players.join(", ")}`);
 };
 
-printTeam("Kepler", "Beth", "Joe", "Susie", "Karen", "Saron");
+const team = {
+  name: "Kepler",
+  coach: "Beth",
+  players: ["Joe", "Susie", "Karen", "Saron"]
+};
+
+printTeam(team.name, team.coach, ...team.players);
+
+let cities = ["Barcelona", "Cape Town", "Berlin"];
+cities = [...cities, "Santiago"];
+console.log(cities);
