bba90489f87b42d460737c25560be7f5fdbd65c5
Gerard Hynes <gkhynes@gmail.com>
2019-04-13 12:44:50 +0100

    Practice working with object spread

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -34,3 +34,35 @@ printTeam(team.name, team.coach, ...team.players);
 let cities = ["Barcelona", "Cape Town", "Berlin"];
 cities = [...cities, "Santiago"];
 console.log(cities);
+
+let house = {
+  bedroom: 2,
+  bathroom: 1.5,
+  yearBuilt: 2015
+};
+
+let newHouse = {
+  basement: false,
+  ...house,
+  bedroom: 3
+};
+
+console.log(house);
+console.log(newHouse);
+
+const person = {
+  name: "Micah",
+  age: 38
+};
+
+const location = {
+  city: "San Diego",
+  country: "US"
+};
+
+const overview = {
+  ...person,
+  ...location
+};
+
+console.log(overview);
