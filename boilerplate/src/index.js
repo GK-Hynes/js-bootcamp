82e513c776cc82bd079b2a4c34af44512e54016d
Gerard Hynes <gkhynes@gmail.com>
2019-04-13 13:34:53 +0100

    Practice working with destructuring

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -17,52 +17,87 @@
 
 // printTeam("Kepler", "Beth", "Joe", "Susie", "Karen", "Saron");
 
-const printTeam = (teamName, coach, ...players) => {
-  console.log(`Team: ${teamName}`);
-  console.log(`Coach: ${coach}`);
-  console.log(`Players: ${players.join(", ")}`);
+// const printTeam = (teamName, coach, ...players) => {
+//   console.log(`Team: ${teamName}`);
+//   console.log(`Coach: ${coach}`);
+//   console.log(`Players: ${players.join(", ")}`);
+// };
+
+// const team = {
+//   name: "Kepler",
+//   coach: "Beth",
+//   players: ["Joe", "Susie", "Karen", "Saron"]
+// };
+
+// printTeam(team.name, team.coach, ...team.players);
+
+// let cities = ["Barcelona", "Cape Town", "Berlin"];
+// cities = [...cities, "Santiago"];
+// console.log(cities);
+
+// let house = {
+//   bedroom: 2,
+//   bathroom: 1.5,
+//   yearBuilt: 2015
+// };
+
+// let newHouse = {
+//   basement: false,
+//   ...house,
+//   bedroom: 3
+// };
+
+// console.log(house);
+// console.log(newHouse);
+
+// const person = {
+//   name: "Micah",
+//   age: 38
+// };
+
+// const location = {
+//   city: "San Diego",
+//   country: "US"
+// };
+
+// const overview = {
+//   ...person,
+//   ...location
+// };
+
+// console.log(overview);
+
+// Destructuring objects
+const todo = {
+  id: "agfhghjkk",
+  text: "Buy clock",
+  completed: false
 };
 
-const team = {
-  name: "Kepler",
-  coach: "Beth",
-  players: ["Joe", "Susie", "Karen", "Saron"]
+const {
+  text: todoText,
+  completed,
+  details: otherDetails = "No details provided",
+  ...others
+} = todo;
+
+// const text = todo.text;
+// const completed = todo.completed;
+
+console.log(todoText);
+console.log(completed);
+console.log(otherDetails);
+console.log(others);
+
+// Destructuring arrays
+const age = [65, 12, 21];
+const [firstAge, ...otherAges] = age;
+
+console.log(firstAge);
+console.log(otherAges);
+
+// Destructuring function parameters
+const printTodo = ({ text, completed }) => {
+  console.log(`${text}: ${completed}`);
 };
-
-printTeam(team.name, team.coach, ...team.players);
-
-let cities = ["Barcelona", "Cape Town", "Berlin"];
-cities = [...cities, "Santiago"];
-console.log(cities);
-
-let house = {
-  bedroom: 2,
-  bathroom: 1.5,
-  yearBuilt: 2015
-};
-
-let newHouse = {
-  basement: false,
-  ...house,
-  bedroom: 3
-};
-
-console.log(house);
-console.log(newHouse);
-
-const person = {
-  name: "Micah",
-  age: 38
-};
-
-const location = {
-  city: "San Diego",
-  country: "US"
-};
-
-const overview = {
-  ...person,
-  ...location
-};
-
-console.log(overview);
+printTodo(todo);
